b20930abc957c5858196ed900c49fed2238018cc
Aiden-Chubet <147650530+Aiden-Chubet@users.noreply.github.com>
2024-01-10 12:57:50 -0500

    Update Auto SFTP.ps1

--- Auto SFTP.ps1
+++ Auto SFTP.ps1
@@ -3,7 +3,7 @@ $PSNativeCommandArgumentPassing = "Legacy"
 & "C:\Program Files (x86)\WinSCP\WinSCP.com" `
   /log="C:\writable\path\to\log\WinSCP.log" /ini=nul `
   /command `
-    "open sftp://SA253:GrzGhiwuO*%408@transport.ochin.org/ -hostkey=`"`"ssh-ed25519 255 8vyt1JBm7OLBHEkiIFX7f5QD0ca2Fyygdcw5Yipjhlw`"`"" `
+    "open sftp://SA253:HIDDEN@transport.ochin.org/ -hostkey=`"`"ssh-ed25519 255 HIDDEN HASH`"`"" `
     "cd /" `
     "lcd S:\OCHIN" `
     "get -neweronly SA253" `
